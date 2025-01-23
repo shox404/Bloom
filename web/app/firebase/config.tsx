63dcfb6a5dc3160a2a035a7dda1932a9159339ec
shox404 <thesh8080@gmail.com>
2025-01-23 22:21:01 +0500

    Update

--- web/app/firebase/config.tsx
+++ web/app/firebase/config.tsx
@@ -1,9 +1,8 @@
 import admin from "firebase-admin";
-
-const serviceAccount = require("./web/keys.json");
+import serviceAccount from "@/keys.json";
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
 
 export const db = admin.firestore();
