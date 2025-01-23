42a36f72ce418a1f2b229a9b035cf515f81bcd4d
shox404 <thesh8080@gmail.com>
2025-01-24 00:05:59 +0500

    Update

--- web/app/firebase/config.tsx
+++ web/app/firebase/config.tsx
@@ -1,8 +1,10 @@
 import admin from "firebase-admin";
 import serviceAccount from "@/keys.json";
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  });
+}
 
 export const db = admin.firestore();
--- web/app/firebase/functions.tsx
+++ web/app/firebase/functions.tsx
@@ -1,6 +1,23 @@
 import { readFileSync } from "fs";
+import { db } from "./config";
 
 export function get_id() {
-  const id = 
-  return id;
+  const id = readFileSync("C:/Users/hp/AppData/LocalLow/bloom.cafe", "utf-8");
+  return eval(id);
+}
+
+export async function get_user(id: number) {
+  const query = await db
+    .collection("users")
+    .where("tg_id", "==", id)
+    .limit(1)
+    .get();
+
+  if (query.empty) {
+    return null;
+  }
+
+  const doc = query.docs[0];
+
+  return { id: doc.id, ...doc.data() };
 }
