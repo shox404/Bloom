f398d00fc782b74ed64de69c07b885c7e421d29b
shox404 <thesh8080@gmail.com>
2025-01-26 01:06:03 +0500

    Update

--- bot/main.py
+++ bot/main.py
@@ -2,13 +2,24 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from handlers import register_routes
+from config import WEB_APP_URL
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 
+async def set_web_app_button():
+    menu_button = {
+        "type": "web_app",
+        "text": "Open App",
+        "web_app": {"url": WEB_APP_URL},
+    }
+    await bot.set_chat_menu_button(menu_button=menu_button)
+
+
 async def main():
     await register_routes(dp)
+    await set_web_app_button()
     await dp.start_polling(bot)
 
 
