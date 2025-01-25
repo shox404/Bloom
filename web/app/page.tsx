c1f67abfbcd9e43d0d94f4d0383e9cad6468b2b9
shox404 <thesh8080@gmail.com>
2025-01-25 11:31:58 +0500

    Update

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -3,7 +3,10 @@ import { get_id, get_user } from "./firebase/functions";
 
 export default function Home() {
   const id: number = get_id();
-  get_user(id);
+
+  get_user(id).then((user) => {
+    console.log(user);
+  });
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
