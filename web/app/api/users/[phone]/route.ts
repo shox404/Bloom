fe6e4fdff341f7b62421543add58a310d258b8dd
shox404 <thesh8080@gmail.com>
2025-02-01 17:51:58 +0500

    Update

--- web/app/api/users/[phone]/route.ts
+++ web/app/api/users/[phone]/route.ts
@@ -1,13 +1,12 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { getUserByPhone } from "@/app/_firebase/functions";
 
-interface Props {
-  params: { phone: string };
-}
-
-export async function GET({ params: { phone } }: Props) {
+export async function GET(
+  _request: NextRequest,
+  { params }: { params: { phone: string } }
+) {
   try {
-    const data = await getUserByPhone(phone);
+    const data = await getUserByPhone(params.phone);
     return NextResponse.json(data);
   } catch {
     return NextResponse.json({ error: "Server error" }, { status: 500 });
