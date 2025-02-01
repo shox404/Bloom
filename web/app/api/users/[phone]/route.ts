22b31d37f764d708b8fd35bfaae78b3672b25666
shox404 <thesh8080@gmail.com>
2025-02-01 18:54:02 +0500

    Update

--- web/app/api/users/[phone]/route.ts
+++ web/app/api/users/[phone]/route.ts
@@ -1,16 +1,20 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { getUserByPhone } from "@/app/_firebase/functions";
 
-export async function GET(
-  _request: NextRequest, // First argument: NextRequest
-  { params }: { params: { phone: string } } // Second argument: params object
-) {
+interface Params {
+  params: { phone: string };
+}
+
+export async function GET({ params: { phone } }: Params) {
   try {
-    if (!params.phone) {
-      return NextResponse.json({ error: "Phone number is required" }, { status: 400 });
+    if (!phone) {
+      return NextResponse.json(
+        { error: "Phone number is required" },
+        { status: 400 }
+      );
     }
 
-    const data = await getUserByPhone(params.phone);
+    const data = await getUserByPhone(phone);
     return NextResponse.json(data);
   } catch {
     return NextResponse.json({ error: "Server error" }, { status: 500 });
