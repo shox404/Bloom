92a192a21a945a3b5000316addd8e52059c384b1
shox404 <thesh8080@gmail.com>
2025-02-13 22:01:32 +0500

    Update

--- web/app/products/[category]/page.tsx
+++ web/app/products/[category]/page.tsx
@@ -22,7 +22,7 @@ export default function Products() {
       {productsByCategory.map((item, index) => (
         <motion.div
           className="card"
-          key={item.id}
+          key={index}
           initial={{ opacity: 0, scale: 0.95 }}
           animate={{ opacity: 1, scale: 1 }}
           exit={{ opacity: 0, scale: 0.95 }}
