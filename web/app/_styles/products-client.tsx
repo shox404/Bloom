331b311451812e6befe8b5a1afad80baf3031f6d
shox404 <thesh8080@gmail.com>
2025-02-14 13:57:55 +0500

    Update

--- web/app/_styles/products-client.tsx
+++ web/app/_styles/products-client.tsx
@@ -2,55 +2,60 @@ import styled from "@emotion/styled";
 import { motion } from "framer-motion";
 
 export const ProductsStyles = styled(motion.div)`
-  padding-block: 20px;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  align-items: start;
-  gap: 10px;
-  .card {
-    width: 270px;
-    background-color: #fff;
-    border-radius: 20px;
-    padding: 5px;
-    .images {
-      border-radius: 16px;
-      .ant-image {
-        min-width: 100%;
+  padding: 20px;
+  background-color: #ebeaef;
+  min-height: 100dvh;
+  .products {
+    margin-top: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-items: start;
+    gap: 10px;
+    .card {
+      width: 270px;
+      background-color: #fff;
+      border-radius: 20px;
+      padding: 5px;
+      .images {
         border-radius: 16px;
-        overflow: hidden;
-        .image {
-          height: 150px;
+        .ant-image {
+          min-width: 100%;
+          border-radius: 16px;
           overflow: hidden;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          object-fit: cover;
-          object-fit: cover;
-          margin: auto;
+          .image {
+            height: 150px;
+            overflow: hidden;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            object-fit: cover;
+            object-fit: cover;
+            margin: auto;
+          }
+        }
+      }
+      .footer {
+        padding: 10px;
+        p {
+          white-space: nowrap;
+          max-width: 100%;
+          text-overflow: ellipsis;
+          overflow: hidden;
+        }
+        button {
+          padding: 10px;
+          height: 35px;
         }
       }
     }
-    .footer {
-      padding: 10px;
-      p {
-        white-space: nowrap;
-        max-width: 100%;
-        text-overflow: ellipsis;
-        overflow: hidden;
-      }
-      button {
-        padding: 10px;
-        height: 35px;
-      }
-    }
-  }
-  @media screen and (max-width: 480px) {
-    padding-inline: 0;
-    .card {
-      width: 100%;
-      .images .image {
-        height: 200px;
+    @media screen and (max-width: 480px) {
+      padding-inline: 0;
+      .card {
+        width: 100%;
+        .images .image {
+          height: 200px;
+        }
       }
     }
   }
--- web/app/products/[category]/page.tsx
+++ web/app/products/[category]/page.tsx
@@ -7,7 +7,7 @@ import { useGetProductByCategoryQuery } from "@/app/_lib/services/products";
 import { useParams } from "next/navigation";
 import { motion } from "framer-motion";
 import { Text, Title } from "@/app/_styles/texts";
-import { Br, Inline } from "@/app/_styles/elements";
+import { Br, Inline, Navbar } from "@/app/_styles/elements";
 import { format } from "@/app/utils";
 import { ProductsStyles } from "@/app/_styles/products-client";
 
@@ -19,34 +19,39 @@ export default function Products() {
 
   return (
     <ProductsStyles>
-      {productsByCategory.map((item, index) => (
-        <motion.div
-          className="card"
-          key={index}
-          initial={{ opacity: 0, scale: 0.95 }}
-          animate={{ opacity: 1, scale: 1 }}
-          exit={{ opacity: 0, scale: 0.95 }}
-          transition={{ duration: 0.3 }}
-        >
-          <div className="images">
-            <AppImage image={item.image} />
-          </div>
-          <div className="footer">
-            <Title>
-              <Tooltip>{item.title}</Tooltip>
-            </Title>
-            <Br px={5} />
-            <Inline y="center">
-              <Text>Price</Text>
-              <Text> $ {format(item.price)}</Text>
-            </Inline>
-            <Br px={5} />
-            <Inline y="end">
-              <Text>{item.category}</Text>
-            </Inline>
-          </div>
-        </motion.div>
-      ))}
+      <Navbar>
+        <Title>Bloom</Title>
+      </Navbar>
+      <div className="products">
+        {productsByCategory.map((item, index) => (
+          <motion.div
+            className="card"
+            key={index}
+            initial={{ opacity: 0, scale: 0.95 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.95 }}
+            transition={{ duration: 0.3 }}
+          >
+            <div className="images">
+              <AppImage image={item.image} />
+            </div>
+            <div className="footer">
+              <Title>
+                <Tooltip>{item.title}</Tooltip>
+              </Title>
+              <Br px={5} />
+              <Inline y="center">
+                <Text>Price</Text>
+                <Text> $ {format(item.price)}</Text>
+              </Inline>
+              <Br px={5} />
+              <Inline y="end">
+                <Text>{item.category}</Text>
+              </Inline>
+            </div>
+          </motion.div>
+        ))}
+      </div>
     </ProductsStyles>
   );
 }
